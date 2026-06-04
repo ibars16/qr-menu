985aff38f765dc26b3093ae4bcd9bb725acbf543
david <david.ibars69@gmail.com>
2026-06-04 14:20:45 +1200

    Improve entities

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,13 +2,17 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\TimestampableTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
 class Category
 {
+    use TimestampableTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -24,15 +28,98 @@ class Category
     #[ORM\Column]
     private bool $active = true;
 
-    #[ORM\OneToMany(mappedBy: 'category', targetEntity: CategoryTranslation::class, cascade: ['persist', 'remove'])]
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: CategoryTranslation::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
     private Collection $translations;
 
-    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class, cascade: ['persist', 'remove'])]
+    #[ORM\OrderBy(['position' => 'ASC'])]
     private Collection $products;
 
     public function __construct()
     {
         $this->translations = new ArrayCollection();
         $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRestaurant(): Restaurant
+    {
+        return $this->restaurant;
+    }
+
+    public function setRestaurant(Restaurant $restaurant): void
+    {
+        $this->restaurant = $restaurant;
+    }
+
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): void
+    {
+        $this->position = $position;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
+
+    public function getTranslations(): Collection
+    {
+        return $this->translations;
+    }
+
+    public function addTranslation(CategoryTranslation $translation): void
+    {
+        if (!$this->translations->contains($translation)) {
+            $this->translations->add($translation);
+            $translation->setCategory($this);
+        }
+    }
+
+    public function removeTranslation(CategoryTranslation $translation): void
+    {
+        $this->translations->removeElement($translation);
+    }
+
+    public function getTranslation(string $locale): ?CategoryTranslation
+    {
+        foreach ($this->translations as $translation) {
+            if ($translation->getLocale() === $locale) {
+                return $translation;
+            }
+        }
+
+        return null;
+    }
+
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): void
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setCategory($this);
+        }
+    }
+
+    public function removeProduct(Product $product): void
+    {
+        $this->products->removeElement($product);
     }
 }
--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -2,22 +2,39 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\TimestampableTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
 class Ingredient
 {
+    use TimestampableTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100, unique: true)]
+    /**
+     * Unique code scoped to the restaurant.
+     * Example: "cheese", "bacon", "flour"
+     * Combined with restaurant_id forms a unique identifier.
+     */
+    #[ORM\Column(length: 100)]
     private string $code;
 
-    #[ORM\OneToMany(mappedBy: 'ingredient', targetEntity: IngredientTranslation::class, cascade: ['persist', 'remove'])]
+    /**
+     * Each ingredient belongs to a restaurant.
+     * This ensures tenant isolation in a multi-restaurant SaaS.
+     */
+    #[ORM\ManyToOne(targetEntity: Restaurant::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Restaurant $restaurant;
+
+    #[ORM\OneToMany(mappedBy: 'ingredient', targetEntity: IngredientTranslation::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
     private Collection $translations;
 
     #[ORM\ManyToMany(targetEntity: Product::class, mappedBy: 'ingredients')]
@@ -34,11 +51,6 @@ class Ingredient
         return $this->id;
     }
 
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-
     public function getCode(): string
     {
         return $this->code;
@@ -49,25 +61,47 @@ class Ingredient
         $this->code = $code;
     }
 
+    public function getRestaurant(): Restaurant
+    {
+        return $this->restaurant;
+    }
+
+    public function setRestaurant(Restaurant $restaurant): void
+    {
+        $this->restaurant = $restaurant;
+    }
+
     public function getTranslations(): Collection
     {
         return $this->translations;
     }
 
-    public function setTranslations(Collection $translations): void
+    public function addTranslation(IngredientTranslation $translation): void
     {
-        $this->translations = $translations;
+        if (!$this->translations->contains($translation)) {
+            $this->translations->add($translation);
+            $translation->setIngredient($this);
+        }
+    }
+
+    public function removeTranslation(IngredientTranslation $translation): void
+    {
+        $this->translations->removeElement($translation);
+    }
+
+    public function getTranslation(string $locale): ?IngredientTranslation
+    {
+        foreach ($this->translations as $translation) {
+            if ($translation->getLocale() === $locale) {
+                return $translation;
+            }
+        }
+
+        return null;
     }
 
     public function getProducts(): Collection
     {
         return $this->products;
     }
-
-    public function setProducts(Collection $products): void
-    {
-        $this->products = $products;
-    }
-
-
 }
