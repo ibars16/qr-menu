aa867b487ab616c6ae00e1f3b8bc2b85e6731acb
david <david.ibars69@gmail.com>
2026-08-30 12:43:15 +1200

    IMprove design

--- config/currencies.php
+++ config/currencies.php
@@ -5,101 +5,121 @@ return [
     'NZD' => [
         'symbol' => '$',
         'name' => 'New Zealand Dollar',
+        'flag' => '🇳🇿',
     ],
 
     'AUD' => [
         'symbol' => '$',
         'name' => 'Australian Dollar',
+        'flag' => '🇦🇺',
     ],
 
     'USD' => [
         'symbol' => '$',
         'name' => 'US Dollar',
+        'flag' => '🇺🇸',
     ],
 
     'CAD' => [
         'symbol' => '$',
         'name' => 'Canadian Dollar',
+        'flag' => '🇨🇦',
     ],
 
     'EUR' => [
         'symbol' => '€',
         'name' => 'Euro',
+        'flag' => '🇪🇺',
     ],
 
     'GBP' => [
         'symbol' => '£',
         'name' => 'British Pound',
+        'flag' => '🇬🇧',
     ],
 
     'JPY' => [
         'symbol' => '¥',
         'name' => 'Japanese Yen',
+        'flag' => '🇯🇵',
     ],
 
     'CNY' => [
         'symbol' => '¥',
         'name' => 'Chinese Yuan',
+        'flag' => '🇨🇳',
     ],
 
     'KRW' => [
         'symbol' => '₩',
         'name' => 'South Korean Won',
+        'flag' => '🇰🇷',
     ],
 
     'THB' => [
         'symbol' => '฿',
         'name' => 'Thai Baht',
+        'flag' => '🇹🇭',
     ],
 
     'SGD' => [
         'symbol' => '$',
         'name' => 'Singapore Dollar',
+        'flag' => '🇸🇬',
     ],
 
     'HKD' => [
         'symbol' => '$',
         'name' => 'Hong Kong Dollar',
+        'flag' => '🇭🇰',
     ],
 
     'CHF' => [
         'symbol' => 'CHF',
         'name' => 'Swiss Franc',
+        'flag' => '🇨🇭',
     ],
 
     'SEK' => [
         'symbol' => 'kr',
         'name' => 'Swedish Krona',
+        'flag' => '🇸🇪',
     ],
 
     'NOK' => [
         'symbol' => 'kr',
         'name' => 'Norwegian Krone',
+        'flag' => '🇳🇴',
     ],
 
     'DKK' => [
         'symbol' => 'kr',
         'name' => 'Danish Krone',
+        'flag' => '🇩🇰',
     ],
 
     'INR' => [
         'symbol' => '₹',
         'name' => 'Indian Rupee',
+        'flag' => '🇮🇳',
     ],
 
     'MXN' => [
         'symbol' => '$',
         'name' => 'Mexican Peso',
+        'flag' => '🇲🇽',
     ],
 
     'BRL' => [
         'symbol' => 'R$',
         'name' => 'Brazilian Real',
+        'flag' => '🇧🇷',
     ],
 
     'AED' => [
         'symbol' => 'د.إ',
         'name' => 'UAE Dirham',
+        'flag' => '🇦🇪',
     ],
 
 ];
